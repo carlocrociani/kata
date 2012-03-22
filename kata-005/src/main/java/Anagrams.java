ca89c089d7347f41d80cd99bd6bf77a80fbc0d1e
gandalf <gandalf@guest-044.dhcp.nx>
2012-03-22 14:02:50 +0100

    a solution ?

--- kata-005/src/main/java/Anagrams.java
+++ kata-005/src/main/java/Anagrams.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Anagrams {
@@ -9,9 +12,56 @@ public class Anagrams {
      * @param word the word you have to find the anagrams
      * @return a Set<String> with the anagrams found
      */
+     private final static String FILE_NAME = "common-passwords.txt";
+    
+    public Set<String> findAnagrams(String word) throws FileNotFoundException {
 
-    public Set<String> findAnagrams(String word) {
-        return null; //TODO: implement me
+        Set<String> words = new HashSet<String>();
+        Set<String> anagrams = new HashSet<String>();
+                
+
+        fetchData(words);
+
+        for (String key : words) {
+            if (key.length() == word.length() && !key.equals(word)) {
+
+                String tmpKey = key;
+
+                for (int i = 0; i < word.length(); i++) {
+
+                    String letter = word.substring(i, i + 1);
+
+                    if (tmpKey.contains(letter)) {
+                        tmpKey = tmpKey.replaceFirst(letter, "");
+
+                    }
+
+                    if (tmpKey.trim().length() == 0) {
+                        anagrams.add(key);
+                    }
+                }
+            }
+
+        }
+        return anagrams;
+
+    }
+
+    private void fetchData(Set<String> words) throws FileNotFoundException {
+
+        InputStream in = ClassLoader.getSystemResourceAsStream(FILE_NAME);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String currentWord;
+
+        try {
+            while ((currentWord = br.readLine()) != null)   {
+                words.add(currentWord);
+                                 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
 }
